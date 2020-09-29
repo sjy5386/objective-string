a4b5eccef9874f659266be23699f27682c1482a6
Seokjin Yoon <sjy5386@gmail.com>
2020-09-30 00:10:30 +0900

    Add constructor and destructor

--- ostring.c
+++ ostring.c
@@ -1,4 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct String {
     struct String *this;
     char *str;
 } String;
+
+String * newString(const char *str) {
+    String *this = (String *) malloc(sizeof(String));
+    int length = strlen(str);
+    this->this = this;
+    this->str = (char *) malloc(sizeof(char) * length);
+    strncpy(this->str, str, length);
+    return this;
+}
+
+void deleteString(String *this) {
+    free(this->str);
+    free(this);
+}
