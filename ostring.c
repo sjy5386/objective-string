1bba0097854065117aa7b3720e0a9608f5ba83b9
Seokjin Yoon <sjy5386@gmail.com>
2020-09-30 00:19:18 +0900

    Add getter and setter

--- ostring.c
+++ ostring.c
@@ -4,7 +4,21 @@
 typedef struct String {
     struct String *this;
     char *str;
+
+    const char * (*get)(struct String *);
+    void (*set)(struct String *, const char *);
 } String;
+
+static const char * getString(String *this) {
+    return this->str;
+}
+
+static void setString(String *this, const char *str) {
+    int length = strlen(str);
+    free(this->str);
+    this->str = (char *) malloc(sizeof(char) * length);
+    strncpy(this->str, str, length);
+}
 
 String * newString(const char *str) {
     String *this = (String *) malloc(sizeof(String));
@@ -12,6 +26,8 @@ String * newString(const char *str) {
     this->this = this;
     this->str = (char *) malloc(sizeof(char) * length);
     strncpy(this->str, str, length);
+    this->get = getString;
+    this->set = setString;
     return this;
 }
 
