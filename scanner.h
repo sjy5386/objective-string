9ffc197baa8f154a225362e62cbaa36ff5c4a778
Seokjin Yoon <sjy5386@gmail.com>
2020-10-07 17:20:25 +0900

    Add struct Scanner

--- scanner.h
+++ scanner.h
@@ -8,4 +8,16 @@
 #ifndef SCANNER_H_
 #define SCANNER_H_
 
+#include <stdio.h>
+
+#define SYSTEM_IN stdin
+#define SYSTEM_OUT stdout
+#define SYSTEM_ERR stderr
+
+typedef struct Scanner {
+    /* field */
+    struct Scanner *this;
+    FILE *src;
+} Scanner;
+
 #endif // SCANNER_H
