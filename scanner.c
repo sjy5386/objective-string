435f4b2cc0cc4205482382fe818bbeb9132eb588
Seokjin Yoon <sjy5386@gmail.com>
2020-10-07 17:10:45 +0900

    Add scanner.c and scanner.h

---
+++ scanner.c
@@ -0,0 +1,8 @@
+/*
+ * scanner.c
+ *
+ *  Created on: Aug 7, 2020
+ *      Author: Seokjin Yoon
+ */
+
+#include "scanner.h"
---
+++ scanner.h
@@ -0,0 +1,11 @@
+/*
+ * scanner.h
+ *
+ *  Created on: Aug 7, 2020
+ *      Author: Seokjin Yoon
+ */
+
+#ifndef SCANNER_H_
+#define SCANNER_H_
+
+#endif // SCANNER_H
